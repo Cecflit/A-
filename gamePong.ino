d79ea60c653469d09280d359a318e039b6bb0774
Hume2 <teratux.mail@gmail.com>
2018-02-27 20:50:03 +0100

    Optimalizován Pong

--- gamePong.ino
+++ gamePong.ino
@@ -21,24 +21,40 @@ void gamePongSetup() {
   Ball.directionX = 1;
   Ball.directionY = 1;
 
+  gamePongDrawScreen();
+
 }
 
 void gamePongLoop() {
 
   gamePongLetMove();
   gamePongMoveBall();
-  gamePongDrawScreen();
+  wait(tickDuration + 1);
 
 }
 
 void gamePongLetMove() {
 
-  if (!digitalRead(buttonUp) && Paddle.positionY > 2)Paddle.positionY--;
-  if (!digitalRead(buttonDown) && Paddle.positionY < 21)Paddle.positionY++;
+  if (!digitalRead(buttonUp) && Paddle.positionY > 2) {
+    clearTile(0, Paddle.positionY + 1);
+    clearTile(23, Paddle.positionY + 1);
+    Paddle.positionY--;
+    drawTile(0, Paddle.positionY - 1);
+    drawTile(23, Paddle.positionY - 1);
+  }
+  if (!digitalRead(buttonDown) && Paddle.positionY < 21) {
+    clearTile(0, Paddle.positionY - 1);
+    clearTile(23, Paddle.positionY - 1);
+    Paddle.positionY++;
+    drawTile(0, Paddle.positionY + 1);
+    drawTile(23, Paddle.positionY + 1);
+  }
 
 }
 
 void gamePongMoveBall() {
+
+  clearTile(Ball.positionX, Ball.positionY);
 
   if ((Ball.positionX == 1 || Ball.positionX == 22) && (Paddle.positionY != Ball.positionY && Paddle.positionY - 1 != Ball.positionY && Paddle.positionY + 1 != Ball.positionY))endGame();
   if ((Ball.positionX == 1 || Ball.positionX == 22) && (Paddle.positionY == Ball.positionY || Paddle.positionY - 1 == Ball.positionY || Paddle.positionY + 1 == Ball.positionY)) {
@@ -83,6 +99,7 @@ void gamePongMoveBall() {
   Ball.positionX += Ball.directionX;
   Ball.positionY += Ball.directionY;
 
+  drawTile(Ball.positionX, Ball.positionY);
 }
 
 void gamePongDrawScreen() {
@@ -102,6 +119,5 @@ void gamePongDrawScreen() {
   drawTile(23, Paddle.positionY);
   drawTile(23, Paddle.positionY - 1);
   drawTile(23, Paddle.positionY + 1);
-  wait(tickDuration + 1);
 
 }
