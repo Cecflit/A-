7fc007a34e92ebee882aafad7fbef1903c2f8eef
Narre <nrrstttpt@gmail.com>
2018-03-04 16:11:19 +0100

    Odkomentování underattack

--- gameUnderAttack.ino
+++ gameUnderAttack.ino
@@ -11,18 +11,6 @@ class rocket {
 
 };
 
-/*rocket Rocket01 = rocket();
-rocket Rocket23 = rocket();
-rocket Rocket45 = rocket();
-rocket Rocket67 = rocket();
-rocket Rocket89 = rocket();
-rocket Rocket1011 = rocket();
-rocket Rocket1213 = rocket();
-rocket Rocket1415 = rocket();
-rocket Rocket1617 = rocket();
-rocket Rocket1819 = rocket();
-rocket Rocket2021 = rocket();
-rocket Rocket2223 = rocket();*/
 rocket Rockets[12];
 
 class victim {
@@ -111,31 +99,7 @@ void gameUnderAttackMoveRockets() {
       Rockets[r].positionX[i]--;
       if (Rockets[r].positionX[i] < 0)Rockets[r].positionX[i] = 23;
     }
-    
-    /*Rocket01.positionX[i]++;
-    if (Rocket01.positionX[i] > 23)Rocket01.positionX[i] = 0;
-    Rocket45.positionX[i]++;
-    if (Rocket45.positionX[i] > 23)Rocket45.positionX[i] = 0;
-    Rocket89.positionX[i]++;
-    if (Rocket89.positionX[i] > 23)Rocket89.positionX[i] = 0;
-    Rocket1213.positionX[i]++;
-    if (Rocket1213.positionX[i] > 23)Rocket1213.positionX[i] = 0;
-    Rocket1617.positionX[i]++;
-    if (Rocket1617.positionX[i] > 23)Rocket1617.positionX[i] = 0;
-    Rocket2021.positionX[i]++;
-    if (Rocket2021.positionX[i] > 23)Rocket2021.positionX[i] = 0;
-    Rocket23.positionX[i]--;
-    if (Rocket23.positionX[i] < 0)Rocket23.positionX[i] = 23;
-    Rocket67.positionX[i]--;
-    if (Rocket67.positionX[i] < 0)Rocket67.positionX[i] = 23;
-    Rocket1011.positionX[i]--;
-    if (Rocket1011.positionX[i] < 0)Rocket1011.positionX[i] = 23;
-    Rocket1415.positionX[i]--;
-    if (Rocket1415.positionX[i] < 0)Rocket1415.positionX[i] = 23;
-    Rocket1819.positionX[i]--;
-    if (Rocket1819.positionX[i] < 0)Rocket1819.positionX[i] = 23;
-    Rocket2223.positionX[i]--;
-    if (Rocket2223.positionX[i] < 0)Rocket2223.positionX[i] = 23;*/
+   
   }
 
 }
@@ -146,36 +110,6 @@ void gameUnderAttackRenderScreen() {
 
     toggleTile(Rockets[r].positionX[i], 2*r);
     toggleTile(Rockets[r].positionX[i], 2*r +1);
-    /*drawTile(Rocket01.positionX[i], 0);
-    drawTile(Rocket01.positionX[i], 1);
-    drawTile(Rocket23.positionX[i], 2);
-    drawTile(Rocket23.positionX[i], 3);
-    drawTile(Rocket45.positionX[i], 4);
-    drawTile(Rocket45.positionX[i], 5);
-    drawTile(Rocket67.positionX[i], 6);
-    drawTile(Rocket67.positionX[i], 7);
-    drawTile(Rocket89.positionX[i], 8);
-    drawTile(Rocket89.positionX[i], 9);
-    drawTile(Rocket1011.positionX[i], 10);
-    drawTile(Rocket1011.positionX[i], 11);
-    drawTile(Rocket1213.positionX[i], 12);
-    drawTile(Rocket1213.positionX[i], 13);
-    drawTile(Rocket1415.positionX[i], 14);
-    drawTile(Rocket1415.positionX[i], 15);
-    drawTile(Rocket1617.positionX[i], 16);
-    drawTile(Rocket1617.positionX[i], 17);
-    drawTile(Rocket1819.positionX[i], 18);
-    drawTile(Rocket1819.positionX[i], 19);
-    drawTile(Rocket2021.positionX[i], 20);
-    drawTile(Rocket2021.positionX[i], 21);
-    drawTile(Rocket2223.positionX[i], 22);
-    drawTile(Rocket2223.positionX[i], 23);*/
-
-    /*if (i >= 2)continue;
-
-    drawTile(11, Victim.positionY[i]);
-    drawTile(12, Victim.positionY[i]);*/
-
   }
   
 }
